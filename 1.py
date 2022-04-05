b18f7f586b92a1059fdbaaf686480477bad6b2cf
schahin0001 <schahinheidari@gmail.com>
2022-04-05 14:56:00 +0200

    first assignment update

--- 1.py
+++ 1.py
@@ -2,11 +2,8 @@ import cv2 as cv
 
 import numpy as np
 
-img = cv.imread("image_processing_Assignment_25/input/flower_input.jpg")
-#img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img = cv.imread("input/flower_input.jpg")
 img = cv.resize(img, (500,500))
-#img = cv.medianBlur(img, 15)
-#img = cv.bilateralFilter(img, 5, 100, 100)
 
 # grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -22,17 +19,17 @@ mask = cv.dilate(canned, kernel, iterations = 1)
 contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 # find big contours
-biggesContours = None
+biggestContours = None
 biggestArea = 0
-for contour in contours:
-    area = cv.contourArea(contour)
+for c in contours:
+    area = cv.contourArea(c)
     if area > biggestArea:
         biggestArea = area
-        biggesContours = contour
+        biggestContours = c
 
 # draw contours
 cropMask = np.zeros_like(mask)
-cv.drawContours(cropMask, [biggesContours], -1, (255), -1)
+cv.drawContours(cropMask, [biggestContours], -1, (255), -1)
 
 # fill in holes
 # inverted
@@ -43,28 +40,24 @@ contours, _ = cv.findContours(inverted, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
 # find small contours
 smallContours = []
-for contour in contours:
-    area = cv.contourArea(contour)
+for c in contours:
+    area = cv.contourArea(c)
     if area < 25000:
         print(area)
-        smallContours.append(contour)
+        smallContours.append(c)
 
 # draw on mask
 cv.drawContours(cropMask, smallContours, -1, (255), -1)
 
 # opening + median blur to smooth jaggies
-pieceChoseMask = cv.erode(cropMask, kernel, iterations = 1)
-pieceChoseMask = cv.dilate(pieceChoseMask, kernel, iterations = 1)
-pieceChoseMask = cv.medianBlur(pieceChoseMask, 5)
+pieceChoosedMask = cv.erode(cropMask, kernel, iterations = 1)
+pieceChoosedMask = cv.dilate(pieceChoosedMask, kernel, iterations = 1)
+pieceChoosedMask = cv.medianBlur(pieceChoosedMask, 5)
 
 # crop image
 crop = np.zeros_like(img)
-crop[pieceChoseMask == 255] = img[pieceChoseMask == 255]
+crop[pieceChoosedMask == 255] = img[pieceChoosedMask == 255]
 
 # show
-#cv.imshow("original", img)
-#cv.imshow("gray", gray)
-#cv.imshow("canny", canned)
-#cv.imshow("mask", crop_mask)
 cv.imshow("cropped", crop)
 cv.waitKey()
