e37496e7a17a9f851f49c2eee5bee5d9419a3dea
schahin0001 <schahinheidari@gmail.com>
2022-04-20 23:26:43 +0200

    add last part of assignment, detect of color in video

--- video.py
+++ video.py
@@ -1,72 +1,35 @@
-import cv2
 import numpy as np
-import pandas as pd
-import argparse
+import cv2 as cv
 
-#Creating argument parser to take image path from command line
-ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True, help="Image Path")
-args = vars(ap.parse_args())
-img_path = args['image']
+cap = cv.VideoCapture(-1)
+font = cv.FONT_ITALIC
+fontSize = 1
+textColor = (0, 0, 0)
+thickness = 1
 
-#Reading the image with opencv
-img = cv2.imread(img_path)
+while True:
 
-#declaring global variables (are used later on)
-clicked = False
-r = g = b = xpos = ypos = 0
-
-#Reading csv file with pandas and giving names to each column
-index=["color","color_name","hex","R","G","B"]
-csv = pd.read_csv('colors.csv', names=index, header=None)
-
-#function to calculate minimum distance from all colors and get the most matching color
-def getColorName(R,G,B):
-    minimum = 10000
-    for i in range(len(csv)):
-        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
-        if(d<=minimum):
-            minimum = d
-            cname = csv.loc[i,"color_name"]
-    return cname
-
-#function to get x,y coordinates of mouse double click
-def draw_function(event, x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b,g,r,xpos,ypos, clicked
-        clicked = True
-        xpos = x
-        ypos = y
-        b,g,r = img[y,x]
-        b = int(b)
-        g = int(g)
-        r = int(r)
-       
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_function)
-
-while(1):
-
-    cv2.imshow("image",img)
-    if (clicked):
-   
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
-        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
-
-        #Creating text string to display( Color name and RGB values )
-        text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-        
-        #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
-
-        #For very light colours we will display text in black colour
-        if(r+g+b>=600):
-            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
-            
-        clicked=False
-
-    #Break the loop when user hits 'esc' key    
-    if cv2.waitKey(20) & 0xFF ==27:
-        break
+    ret, frame = cap.read()
+    frame = cv.resize(frame,(500,500), fx = 0, fy = 0, interpolation=cv.INTER_CUBIC)
+    filter = np.ones((50,50))/ 2500
+    cv.rectangle(frame, (200,200), (300,300), (0,0,0), 4)
+    point = frame[200:300, 200:300]
     
-cv2.destroyAllWindows()
+    res = cv.filter2D(frame, -1, filter)
+    res[200:300, 200:300] = point
+    detect = res[200:300, 200:300]
+
+    if  0 < np.average(detect) <= 85:
+        cv.putText(res, "BLACK", (25, 50), font, fontSize, textColor, thickness)
+    elif 85 < np.average(detect) <= 125:
+        cv.putText(res, "GRAY", (25, 50), font, fontSize, textColor, thickness)
+    else:
+        cv.putText(res, "WHITE", (25, 50), font, fontSize, textColor, thickness) 
+
+
+    cv.imshow("frame", res)
+    if cv.waitKey(25) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv.destroyAllWindows()
